8909e3d62b0f6e34cac1e676a87ff20d8c4297e7
pavelzolotin <pavel.zolotin@goat.digital>
2022-04-19 10:47:13 +0500

    changes in add-poster-btn

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '52de76a2978c6b9ae7d487e5a43af8b5');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fee737dbc7b67276815b9367801cd01c');
